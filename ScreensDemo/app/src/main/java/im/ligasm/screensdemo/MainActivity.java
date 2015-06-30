d2254f6268f0aa1569f25a63c220d75e349a566b
Miroslav Ligas <miroslav.ligas@liferay.com>
2015-06-30 14:53:39 +0100

    Take care of user interaction.

--- ScreensDemo/app/src/main/java/im/ligasm/screensdemo/MainActivity.java
+++ ScreensDemo/app/src/main/java/im/ligasm/screensdemo/MainActivity.java
@@ -4,35 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.liferay.mobile.screens.auth.login.LoginListener;
+import com.liferay.mobile.screens.auth.login.LoginScreenlet;
+import com.liferay.mobile.screens.context.User;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LoginListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instantiate screenlet
+        LoginScreenlet loginScreenlet = (LoginScreenlet) findViewById(R.id.login_screenlet);
+        loginScreenlet.setListener(this);
+    }
+
+
+    @Override
+    public void onLoginSuccess(User user) {
+        TextView t=(TextView)findViewById(R.id.textView);
+        t.setText("Logged in as: " + user.getId());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onLoginFailure(Exception e) {
+        TextView t=(TextView)findViewById(R.id.textView);
+        t.setText("Login unsuccessful: " + e.getMessage());
     }
 }
